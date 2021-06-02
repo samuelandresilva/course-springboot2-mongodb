f4a849aaba758c310701677fca65704b869f3de2
Samuel André <samuel@multitecsistemas.com.br>
2021-06-02 17:00:56 -0300

    Primeiro commit - Projeto Criado

---
+++ src/main/java/com/mongodb/course/CourseMongoApplication.java
@@ -0,0 +1,13 @@
+package com.mongodb.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mongodb/course/CourseMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongodb.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
