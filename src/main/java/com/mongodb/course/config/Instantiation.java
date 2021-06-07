524cbdf9c9e5e4746a23146ce672adbaa788f1e0
Samuel André <samuel@multitecsistemas.com.br>
2021-06-07 13:01:53 -0300

    Operação de instanciação da Base de Dados

---
+++ src/main/java/com/mongodb/course/config/Instantiation.java
@@ -0,0 +1,27 @@
+package com.mongodb.course.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.mongodb.course.domain.User;
+import com.mongodb.course.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository repo;
+	@Override
+	public void run(String... args) throws Exception {
+		repo.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		repo.saveAll(Arrays.asList(maria, alex, bob));
+	}
+}
