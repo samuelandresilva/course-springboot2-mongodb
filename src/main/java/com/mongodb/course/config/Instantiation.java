b361196aadcf174bb0b934b62941708be39fce8d
Samuel André <samuel@multitecsistemas.com.br>
2021-06-08 13:28:05 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/mongodb/course/config/Instantiation.java
+++ src/main/java/com/mongodb/course/config/Instantiation.java
@@ -1,27 +1,45 @@
 package com.mongodb.course.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.mongodb.course.domain.Post;
 import com.mongodb.course.domain.User;
+import com.mongodb.course.dto.AuthorDTO;
+import com.mongodb.course.repositories.PostRepository;
 import com.mongodb.course.repositories.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
-	private UserRepository repo;
+	private UserRepository userReposiroty;
+	
+	@Autowired
+	private PostRepository postReposiroty;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		repo.deleteAll();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		userReposiroty.deleteAll();
+		postReposiroty.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
 		
-		repo.saveAll(Arrays.asList(maria, alex, bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		postReposiroty.saveAll(Arrays.asList(post1, post2));
 	}
 }
